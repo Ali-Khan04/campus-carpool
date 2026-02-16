592d4a16c916daf5ee830a0c99a0a0ffaa2e6ed0
Ali-Khan04 <aliiiiahmaaeed@gmail.com>
2026-02-16 21:58:01 +0500

    feat(map): add live location tracking with watchPositionAsync

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MapView, {
   UrlTile,
   Marker,
@@ -11,32 +11,68 @@ import * as Location from "expo-location";
 import { COLORS } from "@/constants/theme";
 import { LocationCoords } from "@/types/Location";
 
+const DEFAULT_ZOOM = {
+  latitudeDelta: 0.005,
+  longitudeDelta: 0.005,
+};
+
 export default function MapScreen() {
-  const [location, setLocation] = useState<LocationCoords | null>(null);
+  const [location, setLocation] = useState<LocationCoords | null>(null); // to store user's live location
   const [region, setRegion] = useState<Region>({
     latitude: 33.6844, // default location set to Islamabad
     longitude: 73.0479,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+    ...DEFAULT_ZOOM,
+  }); // to which and how much region to show on map
+  const mapRef = useRef<MapView>(null);
 
   useEffect(() => {
+    let subscription: Location.LocationSubscription | null = null;
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      let { status } = await Location.requestForegroundPermissionsAsync(); // ask for permission
       if (status !== "granted") {
         Alert.alert("Permission denied", "Location permission is required");
         return;
       }
 
-      let userLocation = await Location.getCurrentPositionAsync({});
-      setLocation(userLocation.coords);
-      setRegion({
-        latitude: userLocation.coords.latitude,
-        longitude: userLocation.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      });
+      // to track live postion , check again
+      const servicesEnabled = await Location.hasServicesEnabledAsync();
+      if (!servicesEnabled) {
+        Alert.alert("Location Services Off", "Please enable location Services");
+        return;
+      }
+
+      //let userLocation = await Location.getCurrentPositionAsync({}); //get user's current location
+      // watch for live updates on user's location
+      subscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 2000, // update every 2 seconds
+          distanceInterval: 2, // or when user moves 2 meters
+        },
+        (loc) => {
+          setLocation(loc.coords);
+
+          setRegion((prev) => {
+            if (prev.latitude === 33.6844 && prev.longitude === 73.0479) {
+              const newRegion = {
+                latitude: loc.coords.latitude,
+                longitude: loc.coords.longitude,
+                ...DEFAULT_ZOOM,
+              };
+
+              mapRef.current?.animateToRegion(newRegion, 800);
+
+              return newRegion;
+            }
+
+            return prev;
+          });
+        },
+      );
     })();
+    return () => {
+      if (subscription) subscription.remove();
+    };
   }, []);
 
   return (
@@ -48,21 +84,22 @@ export default function MapScreen() {
         }}
       />
       <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_DEFAULT}
+        <MapView //to render the actual map
+          provider={PROVIDER_DEFAULT} // use the device native map provider
           style={styles.map}
-          region={region}
+          initialRegion={region} // state variable region for only displaying intially
           showsUserLocation={true}
           showsMyLocationButton={true}
+          ref={mapRef}
         >
-          <UrlTile
-            urlTemplate="https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
+          <UrlTile // for custom map styling
+            urlTemplate="https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png" // custom map template form openStreet
             maximumZ={19}
             minimumZ={1}
             flipY={false}
             tileSize={1024}
           />
-          {location && (
+          {location && ( // if user location is true then show marker at user's current location
             <Marker
               coordinate={{
                 latitude: location.latitude,
